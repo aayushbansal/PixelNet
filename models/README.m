26aae39cf0373d0488e08c29d7b573d0d87040e4
Aayush Bansal <aayushb@devbox1.vision.cs.cmu.edu>
2017-03-14 14:06:07 -0400

    first update

--- models/README.m
+++
@@ -1,8 +0,0 @@
-% all the models that we need -- AB
-% 1. 224x224 segmentation and surface normal estimation (MLP)
-% 2. above with Linear + bn
-% 3. above for training from scratch
-% 4. surface normal estimation (new version - single scale)
-% 5. semantic segmentation - PASCAL Context Model
-% 6. Edge detection - BSDS model
-
